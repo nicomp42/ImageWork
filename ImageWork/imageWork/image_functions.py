65665482a33e56a4b815dd6349c5d058bdee783f
nicomp42 <nicholdw@ucmail.uc.edu>
2021-07-11 19:44:54 -0400

    Added some functions and code in the main to demo them

--- ImageWork/imageWork/image_functions.py
+++ ImageWork/imageWork/image_functions.py
@@ -3,3 +3,39 @@ Created on Jul 11, 2021
 
 @author: nicomp
 '''
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
+
+
+'''
+    Load an image file from disk
+    :param filename: The file to load
+    :return the image object
+'''
+def load_image( filename ) :
+    try:
+        myimage = Image.open(filename)
+        myimage.load()
+        return myimage
+    except:
+        return None
+
+
+def save_image( imageObject, outfilename ) :
+    '''
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    '''
+    imageObject.save( outfilename )
+
+def blur_image(imageObject):
+    # Doesn't look too different but it does blur a little. Depends on the original image
+    blurred = imageObject.filter(ImageFilter.BLUR)
+    # blurred = imageObject.filter(ImageFilter.EMBOSS)    # This produces dramatic results. 
+    # blurred = imageObject.filter(ImageFilter.DETAIL)    # Doesn't look much different 
+    return blurred
+
+
--- ImageWork/imageWork/main.py
+++ ImageWork/imageWork/main.py
@@ -4,5 +4,29 @@ Created on Jul 11, 2021
 @author: nicomp
 '''
 
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
+# Following goes in main only
+from image_functions import *
+
+
 if __name__ == '__main__':
-    pass
+    # open an image file. The default path is where this python module is
+    #im = Image.open("SiriusAndViolet.jpg")
+    #print(im.width, im.height, im.mode, im.format)  # Display some info about the image
+
+    #my_image = load_image("SiriusAndViolet.jpg")
+    #if my_image != None:
+    #    my_image.show(my_image)
+    #else:
+    #    print("Unable to open file.")
+
+    #im = Image.open("SiriusAndViolet.jpg")  
+    #im_c = im.crop((200,300,400,500)) # (left, top, right, bottom) it's a tuple!
+    #im_c.show()
+
+
+    my_image = blur_image(load_image("SiriusAndViolet.jpg"))    # Nested function calls
+    my_image.show()
